97736bcb49a94c92becbce6f1ec3a63db2853ad9
Guiwoo <park.guiwoo@hotmail.com>
2021-11-10 17:33:31 +0900

    infinity_icons with setValue

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Animated, PanResponder, View } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
 
 const Container = styled.View`
   flex: 1;
@@ -12,12 +13,29 @@ const Container = styled.View`
 
 const Card = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
-  width: 300px;
-  height: 300px;
+  width: 250px;
+  height: 250px;
   justify-content: center;
   align-items: center;
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+  position: absolute;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  margin-top: 100px;
+  flex: 1;
+`;
+
+const Btn = styled.TouchableOpacity`
+  margin: 0px 10px;
+`;
+
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function App() {
@@ -29,7 +47,11 @@ export default function App() {
     outputRange: ["-15deg", "15deg"],
     extrapolate: "clamp",
   });
-
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.8, 1],
+    extrapolate: "clamp",
+  });
   //Animations
   const onPressIn = () =>
     Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
@@ -41,7 +63,16 @@ export default function App() {
     toValue: 0,
     useNativeDriver: true,
   });
-
+  const goLeft = Animated.spring(position, {
+    toValue: -400,
+    tension: 5,
+    useNativeDriver: true,
+  });
+  const goRight = Animated.spring(position, {
+    toValue: 400,
+    tension: 5,
+    useNativeDriver: true,
+  });
   //Panresponder
   const panResponder = useRef(
     PanResponder.create({
@@ -49,15 +80,9 @@ export default function App() {
       onPanResponderGrant: () => onPressIn(),
       onPanResponderRelease: (_, { dx }) => {
         if (dx < -210) {
-          Animated.spring(position, {
-            toValue: -400,
-            useNativeDriver: true,
-          }).start();
+          goLeft.start();
         } else if (dx > 210) {
-          Animated.spring(position, {
-            toValue: 400,
-            useNativeDriver: true,
-          }).start();
+          goRight.start();
         } else {
           Animated.parallel([onPressOut, onGo]).start();
         }
@@ -67,20 +92,49 @@ export default function App() {
       },
     })
   ).current;
+  //state
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  };
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  };
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  };
   return (
     <Container>
-      <Card
-        {...panResponder.panHandlers}
-        style={{
-          transform: [
-            { scale },
-            { translateX: position },
-            { rotateZ: rotation },
-          ],
-        }}
-      >
-        <Ionicons name="pizza" size={98} color="#ff5e57" />
-      </Card>
+      <CardContainer>
+        <Card
+          style={{
+            transform: [{ scale: secondScale }],
+          }}
+        >
+          <Ionicons name={icons[index + 1]} size={98} color="#ff5e57" />
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={{
+            transform: [
+              { scale },
+              { translateX: position },
+              { rotateZ: rotation },
+            ],
+          }}
+        >
+          <Ionicons name={icons[index]} size={98} color="#ff5e57" />
+        </Card>
+      </CardContainer>
+      <ButtonContainer>
+        <Btn onPress={closePress}>
+          <Ionicons name="close-circle" color="white" size={58} />
+        </Btn>
+        <Btn onPress={checkPress}>
+          <Ionicons name="checkmark-circle" color="white" size={58} />
+        </Btn>
+      </ButtonContainer>
     </Container>
   );
 }
